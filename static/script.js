05f1cd33d267d4952509b8a7922314e96f87b648
xinx666 <misha139misha@mail.ru>
2026-08-03 20:30:43 +0700

    Заказы отправляются через бота

--- static/script.js
+++ static/script.js
@@ -800,8 +800,8 @@ function sendOrderToBot(orderText) {
         user_id: currentUserId
     });
     
-    // Отправляем заказ на веб-сервер
-    fetch('https://shaurma-webapp.onrender.com/webapp_data', {
+    // Отправляем заказ НА БОТА (shaurma-bott), а не на веб-приложение
+    fetch('https://shaurma-bott.onrender.com/webapp_data', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: payload
